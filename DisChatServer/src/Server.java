0747e3815885a24e4ed16bf37a283fb94f51cfbd
fa <fa@fa-PC.nyc.rr.com>
2014-01-25 10:19:17 -0500

    db test

--- DisChatServer/src/Server.java
+++ DisChatServer/src/Server.java
@@ -1,7 +1,12 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.PreparedStatement;
 
 
 public class Server {
@@ -14,42 +19,55 @@ public class Server {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		new Server();
+//		new Server();
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dischatdatabase", "root", "2289");
+		
+		java.sql.PreparedStatement statement = con.prepareStatement("select * from name");
+		
+		ResultSet result = statement.executeQuery();
+		
+		while(result.next())
+		{
+			System.out.println(result.getString(2));
+		}
 		
 	}
 	
-	public Server(){
-		
-		try {
-			
-			// Create a server socket
-			serverSocket = new ServerSocket(8885);
-			
-			// Listen for a connection request
-			socket = serverSocket.accept();
-			
-			// Create data input and output streams
-			inputFromMobile = new DataInputStream(socket.getInputStream());
-			outputToMobile = new DataOutputStream(socket.getOutputStream());
-			
-			
-			while(true){
-				
-				String message = inputFromMobile.readUTF();
-				
-				outputToMobile.writeUTF(message);
-				
-			}
-			
-			
-		} catch (IOException e) {
-			
-		}
-		
-		
-		
-	}
+//	public Server(){
+//		
+//		try {
+//			
+//			// Create a server socket
+//			serverSocket = new ServerSocket(8885);
+//			
+//			// Listen for a connection request
+//			socket = serverSocket.accept();
+//			
+//			// Create data input and output streams
+//			inputFromMobile = new DataInputStream(socket.getInputStream());
+//			outputToMobile = new DataOutputStream(socket.getOutputStream());
+//			
+//			
+//			while(true){
+//				
+//				String message = inputFromMobile.readUTF();
+//				
+//				outputToMobile.writeUTF(message);
+//				
+//			}
+//			
+//			
+//		} catch (IOException e) {
+//			
+//		}
+//		
+//		
+//		
+//	}
 
 }
